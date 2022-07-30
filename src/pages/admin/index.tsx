dd151542d09aff1b5e1c91ce5a71ceca2ae0be30
BrenerViotto <brener.viotto@a2wplataforma.com.br>
2022-07-30 14:43:14 -0300

    validação

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,8 +1,18 @@
 import Head from 'next/head';
+import router from 'next/router';
+import { useEffect } from 'react';
 
 import Footer from '../../components/Footer';
 import { HeaderAdmin } from '../../components/HeaderAdmin';
 import { HomeContainer } from '../../styles/HomeStyles';
+
+
+useEffect (() => {
+  const token = localStorage.getItem('user');
+  if (!token) {
+      router.push('/');
+  }
+} , []);
 
 
 export default function Home() {
