32c3da101416348be800bba90479e1ac03a6f8c5
BrenerViotto <brenerviotto@gmail.com>
2023-01-23 16:27:22 -0300

    fix

--- src/pages/api/posts.tsx
+++ src/pages/api/posts.tsx
@@ -10,6 +10,8 @@ const prisma = new PrismaClient();
 
 export default async (req, res) => {
 
+    console.log(req)
+
 
      if(req.method === 'GET'){
             const token = req.headers.authorization.split(' ')[1];
@@ -27,12 +29,15 @@ export default async (req, res) => {
 
         
     } else if (req.method === 'POST'){
+        console.log(req.body)
         const token = req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET, async function(err, decoded) {
             if (err) {
+                console.log()
                 res.status(401).json({error: 'Token expired'});
             } else {
                 const { title, content, description, image } = req.body;
+                console.log(`Valido`)
                 await prisma.post.create({
                     data: {
                       title: title,
@@ -42,7 +47,9 @@ export default async (req, res) => {
                       authorId: decoded.id,
                       createdAt: new Date(),
                       updatedAt: new Date(),
-                    }})
+                    }}).then((post) => {
+                        res.status(200).json({post});
+                    }).catch((err) => {})
                 }
             })
 
