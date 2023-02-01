e54f78981fb7680a88ce94abe5d67a09bc4d50c7
Brener Viotto <brenerviotto@gmail.com>
2023-02-01 19:03:33 -0300

    teste

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -9,10 +9,6 @@ const Posts = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(10);
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-
   // Change page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -53,7 +49,7 @@ const Posts = () => {
           Criar Novo Post
         </CreatePostButton>
       </ButtonContainer>
-      {currentPosts.map((post) => (
+      {posts.map((post) => (
         <PostContainer key={post.id}>
           <Title>{post.title}</Title>
           <Image src={post.image} alt={post.title} />
